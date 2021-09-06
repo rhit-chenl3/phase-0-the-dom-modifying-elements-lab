e905defed35ac84f49967f53998b0792e5f9768f
Leo Chen <chenl3@rose-hulman.edu>
2021-09-06 17:03:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+// const element = document.getElementById("main");
+// element.remove();
+
+// const selector = document.querySelector("main");
+// selector.remove(); 
+
+main.remove(main);
+
+const newHeader = document.createElement('h1'); 
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+victory.innerHTML = "<h1> Leo is the champion </h1>";
